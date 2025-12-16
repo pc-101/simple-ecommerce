17cedcb7c1c56d4c6595c886749383764879f4c2
Peter Chea <longkchea@gmail.com>
2025-12-16 03:46:58 -0800

    Remove sign in button on home page after user has already signed in

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
-export default function HomePage(){
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
+export default async function HomePage(){
+  const session = await getServerSession(authOptions);
+  console.log(authOptions)
   return (
     <div className="mx-auto max-w-2xl space-y-6">
       <h1 className="text-2xl font-semibold">E‑Commerce Storefront</h1>
       <p className="text-slate-600 dark:text-slate-300">Catalog, filters, cart, and auth. Code-splitting + image lazy-load for snappy UX.</p>
       <div className="flex gap-2">
         <Link href="/catalog" className="btn">Browse Catalog</Link>
-        <Link href="/signin" className="btn">Sign In</Link>
+        {!session && <Link href="/signin" className="btn">Sign In</Link>}
       </div>
     </div>
   );
